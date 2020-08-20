dfcf3cdbee5a1ebfbfe3a71cb1e55f18d91c8ced
Heather Nuffer <zzzuzz@gmail.com>
2020-08-20 14:37:39 -0600

    Adds another implementation of ex5 (the filename query one), for future reference

--- hashtables/ex5/ex5.py
+++ hashtables/ex5/ex5.py
@@ -1,4 +1,8 @@
-def finder(files, queries):
+from typing import List
+from os.path import basename
+
+
+def finder2(files, queries):
     """
     Given a list of files and a list of queries, return the files that contain the queries
 
@@ -6,6 +10,7 @@ def finder(files, queries):
     Then go through list of queries and look them up in the dict. If an entry exists, append
     the value to the result.
     """
+
     files_dict = dict()
     for file in files:
         file_split = file.split('/')
@@ -25,6 +30,35 @@ def finder(files, queries):
     # print(result)
     result = [val for sublist in result for val in sublist]
     return result
+
+
+def finder(files: List[str], queries: List[str]):
+    # Here is an alternate implementation. For this version, the lookup element is the queries, not the files.
+
+    # The challenge states that dictionaries must be used and not sets.
+    # If it didn't say that, I'd use a set here instead of a dictionary:
+    queries_dict = {}
+    output = []
+    for query in queries:
+        queries_dict[query] = True
+
+    for file in files:
+        if get_end_of_path(file) in queries_dict:
+            output.append(file)
+    return output
+
+# Two additional ways to get the end of the path of the files:
+
+
+def get_end_of_path(filename: str) -> str:
+    index = filename.rfind('/')
+    if index == -1:
+        return filename
+    return filename[index+1:]
+
+
+def get_end_of_path2(filename: str) -> str:
+    return basename(filename)
 
 
 if __name__ == "__main__":
